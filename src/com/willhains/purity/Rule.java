597553f83614593597f506599d04e7ec90a51ff9
Will Hains <will@willhains.com>
2019-01-01 08:09:37 +0900

    Add framework for validating raw values.

---
+++ src/com/willhains/purity/Rule.java
@@ -0,0 +1,74 @@
+package com.willhains.purity;
+
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+/**
+ * Normalise and/or validate raw data before it is wrapped in a {@link Single} or other {@link Value} object.
+ *
+ * @param <Raw> the raw type to be validated/normalised.
+ */
+@FunctionalInterface
+public @Value interface Rule<Raw> extends Function<Raw, Raw>
+{
+	/** Combine multiple rules into a single rule. */
+	static <Raw> Rule<Raw> rules(final Rule<Raw>... combiningRules)
+	{
+		return raw ->
+		{
+			Raw result = raw;
+			for(final Rule<Raw> rule: combiningRules) result = rule.apply(result);
+			return result;
+		};
+	}
+	
+	/**
+	 * Convert the {@link Predicate} `condition` into a {@link Rule} where `condition` must evaluate to `true`.
+	 *
+	 * @param condition the raw value must satisfy this condition to be valid.
+	 * @param errorMessageFactory generate the text of {@link IllegalArgumentException} when the condition is not met.
+	 * @param <Raw> the raw value type to be validated.
+	 * @return a {@link Rule} that passes the value through as-is, unless `condition` is not satisfied.
+	 */
+	static <Raw> Rule<Raw> validOnlyIf(
+		final Predicate<? super Raw> condition,
+		final Function<? super Raw, String> errorMessageFactory)
+	{
+		return raw ->
+		{
+			if(condition.test(raw)) return raw;
+			throw new IllegalArgumentException(errorMessageFactory.apply(raw));
+		};
+	}
+	
+	/**
+	 * Convert the {@link Predicate} `condition` into a {@link Rule} where `condition` must evaluate to `false`.
+	 *
+	 * @param condition the raw value must not satisfy this condition to be valid.
+	 * @param errorMessageFactory generate the text of {@link IllegalArgumentException} when the condition is met.
+	 * @param <Raw> the raw value type to be validated.
+	 * @return a {@link Rule} that passes the value through as-is, unless `condition` is satisfied.
+	 */
+	static <Raw> Rule<Raw> validUnless(
+		final Predicate<? super Raw> condition,
+		final Function<? super Raw, String> errorMessageFactory)
+	{
+		return validOnlyIf(condition.negate(), errorMessageFactory);
+	}
+	
+	/**
+	 * @see #validOnlyIf(Predicate,Function)
+	 */
+	static <Raw> Rule<Raw> validOnlyIf(final Predicate<? super Raw> condition, final String errorMessage)
+	{
+		return validOnlyIf(condition, $ -> errorMessage);
+	}
+	
+	/**
+	 * @see #validUnless(Predicate,Function)
+	 */
+	static <Raw> Rule<Raw> validUnless(final Predicate<? super Raw> condition, final String errorMessage)
+	{
+		return validOnlyIf(condition.negate(), errorMessage);
+	}
+}
