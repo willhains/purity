f9e2ef33d13e741c530a92b420dd7ece6385b547
Will Hains <will@willhains.com>
2019-01-03 00:24:06 +0900

    Introduce `Plural<Element>`.

---
+++ src/com/willhains/purity/Plural.java
@@ -0,0 +1,157 @@
+package com.willhains.purity;
+
+import java.util.*;
+
+import static java.util.Collections.singletonList;
+import static java.util.Collections.unmodifiableList;
+
+/**
+ * An immutable ordered collection of elements, that can be treated as a {@link Value}, so long as the {@link Element}s
+ * are {@link Value}s.
+ *
+ * @author willhains
+ * @param <Element> the type of each element contained within.
+ */
+public final @Value class Plural<@Value Element>
+{
+	private static final Plural<?> _EMPTY = new Plural<>(new Reading<>(Collections.emptyList()));
+	
+	/** @return an empty {@link Plural}. */
+	public static <@Value Element> Plural<Element> empty()
+	{
+		@SuppressWarnings("unchecked") final Plural<Element> empty = (Plural<Element>)_EMPTY;
+		return empty;
+	}
+	
+	/**
+	 * @param e1 (e2, e3, ...) elements of the collection.
+	 * @param <E> the type of elements.
+	 * @return a {@link Plural} containing the specified elements.
+	 */
+	// @formatter:off
+	public static <@Value E> Plural<E> of(E e1) { return new Plural<>(new Reading<>(singletonList(e1))); }
+	public static <@Value E> Plural<E> of(E e1, E e2) { return _wrap(e1, e2); }
+	public static <@Value E> Plural<E> of(E e1, E e2, E e3) { return _wrap(e1, e2, e3); }
+	public static <@Value E> Plural<E> of(E e1, E e2, E e3, E e4) { return _wrap(e1, e2, e3, e4); }
+	public static <@Value E> Plural<E> of(E e1, E e2, E e3, E e4, E e5) { return _wrap(e1, e2, e3, e4, e5); }
+	public static <@Value E> Plural<E> of(E e1, E e2, E e3, E e4, E e5, E e6) { return _wrap(e1, e2, e3, e4, e5, e6); }
+	public static <@Value E> Plural<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7) { return _wrap(e1, e2, e3, e4, e5, e6, e7); }
+	public static <@Value E> Plural<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8) { return _wrap(e1, e2, e3, e4, e5, e6, e7, e8); }
+	public static <@Value E> Plural<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8, E e9) { return _wrap(e1, e2, e3, e4, e5, e6, e7, e8, e9); }
+	public static <@Value E> Plural<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8, E e9, E e10) { return _wrap(e1, e2, e3, e4, e5, e6, e7, e8, e9, e10); }
+	public static <@Value E> Plural<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8, E e9, E e10, E e11) { return _wrap(e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11); }
+	public static <@Value E> Plural<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8, E e9, E e10, E e11, E e12) { return _wrap(e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12); }
+	public static <@Value E> Plural<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8, E e9, E e10, E e11, E e12, E e13) { return _wrap(e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13); }
+	public static <@Value E> Plural<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8, E e9, E e10, E e11, E e12, E e13, E e14) { return _wrap(e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14); }
+	public static <@Value E> Plural<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8, E e9, E e10, E e11, E e12, E e13, E e14, E e15) { return _wrap(e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15); }
+	public static <@Value E> Plural<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8, E e9, E e10, E e11, E e12, E e13, E e14, E e15, E e16) { return _wrap(e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15, e16); }
+	public static <@Value E> Plural<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8, E e9, E e10, E e11, E e12, E e13, E e14, E e15, E e16, E e17) { return _wrap(e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15, e16, e17); }
+	public static <@Value E> Plural<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8, E e9, E e10, E e11, E e12, E e13, E e14, E e15, E e16, E e17, E e18) { return _wrap(e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15, e16, e17, e18); }
+	public static <@Value E> Plural<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8, E e9, E e10, E e11, E e12, E e13, E e14, E e15, E e16, E e17, E e18, E e19) { return _wrap(e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15, e16, e17, e18, e19); }
+	public static <@Value E> Plural<E> of(E e1, E e2, E e3, E e4, E e5, E e6, E e7, E e8, E e9, E e10, E e11, E e12, E e13, E e14, E e15, E e16, E e17, E e18, E e19, E e20, E... more)
+	// @formatter:on
+	{
+		final int argsLength = 20;
+		if(more.length > Integer.MAX_VALUE - argsLength) throw new IllegalArgumentException("too many elements");
+		final Object[] objectArray = new Object[argsLength + more.length];
+		objectArray[0] = e1;
+		objectArray[1] = e2;
+		objectArray[2] = e3;
+		objectArray[3] = e4;
+		objectArray[4] = e5;
+		objectArray[5] = e6;
+		objectArray[6] = e7;
+		objectArray[7] = e8;
+		objectArray[8] = e9;
+		objectArray[9] = e10;
+		objectArray[10] = e11;
+		objectArray[11] = e12;
+		objectArray[12] = e13;
+		objectArray[13] = e14;
+		objectArray[14] = e15;
+		objectArray[15] = e16;
+		objectArray[16] = e17;
+		objectArray[17] = e18;
+		objectArray[18] = e19;
+		objectArray[19] = e20;
+		System.arraycopy(more, 0, objectArray, argsLength, more.length);
+		@SuppressWarnings("unchecked") final E[] elementArray = (E[])objectArray;
+		return _wrap(elementArray);
+	}
+	
+	// Wrap the elements array in a Plural without defensive copy
+	@SafeVarargs
+	private static <@Value Element> Plural<Element> _wrap(final Element... elements)
+	{
+		switch(elements.length)
+		{
+			case 0: return empty();
+			case 1: return of(elements[0]);
+			default: return new Plural<>(new Reading<>(Arrays.asList(elements)));
+		}
+	}
+	
+	/** Copy {@code elements} into a new {@link Plural} value. */
+	@SafeVarargs
+	public static <@Value Element> Plural<Element> copy(final Element... elements)
+	{
+		return _wrap(elements.clone());
+	}
+	
+	/** Copy {@code elements} into a new {@link Plural} value. */
+	public static <@Value Element> Plural<Element> copy(final Iterable<Element> elements)
+	{
+		if(elements instanceof Collection) return copy((Collection<Element>)elements);
+		final ArrayList<Element> list = new ArrayList<>();
+		elements.forEach(list::add);
+		if(list.isEmpty()) return empty();
+		return new Plural<>(new Reading<>(list));
+	}
+	
+	/** Copy {@code elements} into a new {@link Plural} value. */
+	public static <@Value Element> Plural<Element> copy(final Collection<Element> elements)
+	{
+		if(elements.isEmpty()) return empty();
+		return new Plural<>(new Reading<>(new ArrayList<>(elements)));
+	}
+	
+	private MutationState<Element> _state;
+	private Plural(final MutationState<Element> state) { _state = state; }
+	
+	@Override
+	public boolean equals(final Object other)
+	{
+		if(other == this) return true;
+		if(other == null) return false;
+		if(!this.getClass().equals(other.getClass())) return false;
+		@SuppressWarnings("unchecked") final Plural<?> that = (Plural<?>)other;
+		return Single.equals(this._prepareForRead(), that._prepareForRead());
+	}
+	
+	@Override public int hashCode() { return Single.hashCode(_prepareForRead()); }
+	@Override public String toString() { return Single.toString(_prepareForRead()); }
+	
+	private @Value interface MutationState<@Value Element>
+	{
+		Reading<Element> prepareForRead();
+	}
+	
+	// The state where all mutations have been applied to the underlying collection, and it can now be read
+	private static final @Value class Reading<@Value Element> implements MutationState<Element>
+	{
+		private final List<Element> _elements;
+		Reading(final List<Element> elements) { _elements = elements; }
+		@Override public Reading<Element> prepareForRead() { return this; }
+	}
+	
+	// Apply all mutations, collapsing them to the resulting collection, then return that collection
+	private List<Element> _prepareForRead()
+	{
+		final Reading<Element> state = _state.prepareForRead();
+		_state = state;
+		return state._elements;
+	}
+	
+	/** @return an immutable {@link List} containing the elements of this {@link Plural}. */
+	public List<Element> asList() { return unmodifiableList(_prepareForRead()); }
+}
---
+++ test/com/willhains/purity/PluralTest.java
@@ -0,0 +1,199 @@
+package com.willhains.purity;
+
+import org.junit.Test;
+
+import java.util.*;
+
+import static java.util.stream.Collectors.joining;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+public class PluralTest
+{
+	@Test
+	public void shouldCreateEmpty()
+	{
+		final Plural<Object> x = Plural.empty();
+		final Plural<Object> y = Plural.copy(new Object[0]);
+		assertTrue(x.asList().isEmpty());
+		assertTrue(y.asList().isEmpty());
+		assertEquals(x, y);
+		assertEquals(x.hashCode(), y.hashCode());
+	}
+	
+	@Test
+	public void shouldCreateWithOneElement()
+	{
+		final Plural<String> x = Plural.of("a");
+		final Plural<String> y = Plural.of("b");
+		assertThat(x.asList().size(), is(1));
+		assertNotEquals(x, y);
+		assertThat(x.toString(), containsString("a"));
+	}
+	
+	@Test
+	public void shouldCreateWithUpToTwentyElements()
+	{
+		// @formatter:off
+		final Plural<String> x2 = Plural.of("a","b");
+		final Plural<String> x3 = Plural.of("a","b","c");
+		final Plural<String> x4 = Plural.of("a","b","c","d");
+		final Plural<String> x5 = Plural.of("a","b","c","d","e");
+		final Plural<String> x6 = Plural.of("a","b","c","d","e","f");
+		final Plural<String> x7 = Plural.of("a","b","c","d","e","f","g");
+		final Plural<String> x8 = Plural.of("a","b","c","d","e","f","g","h");
+		final Plural<String> x9 = Plural.of("a","b","c","d","e","f","g","h","i");
+		final Plural<String> x10 = Plural.of("a","b","c","d","e","f","g","h","i","j");
+		final Plural<String> x11 = Plural.of("a","b","c","d","e","f","g","h","i","j","k");
+		final Plural<String> x12 = Plural.of("a","b","c","d","e","f","g","h","i","j","k","l");
+		final Plural<String> x13 = Plural.of("a","b","c","d","e","f","g","h","i","j","k","l","m");
+		final Plural<String> x14 = Plural.of("a","b","c","d","e","f","g","h","i","j","k","l","m","n");
+		final Plural<String> x15 = Plural.of("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o");
+		final Plural<String> x16 = Plural.of("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p");
+		final Plural<String> x17 = Plural.of("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q");
+		final Plural<String> x18 = Plural.of("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r");
+		final Plural<String> x19 = Plural.of("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s");
+		final Plural<String> x20 = Plural.of("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t");
+		assertThat(x2.asList().stream().collect(joining("")), is("ab"));
+		assertThat(x3.asList().stream().collect(joining("")), is("abc"));
+		assertThat(x4.asList().stream().collect(joining("")), is("abcd"));
+		assertThat(x5.asList().stream().collect(joining("")), is("abcde"));
+		assertThat(x6.asList().stream().collect(joining("")), is("abcdef"));
+		assertThat(x7.asList().stream().collect(joining("")), is("abcdefg"));
+		assertThat(x8.asList().stream().collect(joining("")), is("abcdefgh"));
+		assertThat(x9.asList().stream().collect(joining("")), is("abcdefghi"));
+		assertThat(x10.asList().stream().collect(joining("")), is("abcdefghij"));
+		assertThat(x11.asList().stream().collect(joining("")), is("abcdefghijk"));
+		assertThat(x12.asList().stream().collect(joining("")), is("abcdefghijkl"));
+		assertThat(x13.asList().stream().collect(joining("")), is("abcdefghijklm"));
+		assertThat(x14.asList().stream().collect(joining("")), is("abcdefghijklmn"));
+		assertThat(x15.asList().stream().collect(joining("")), is("abcdefghijklmno"));
+		assertThat(x16.asList().stream().collect(joining("")), is("abcdefghijklmnop"));
+		assertThat(x17.asList().stream().collect(joining("")), is("abcdefghijklmnopq"));
+		assertThat(x18.asList().stream().collect(joining("")), is("abcdefghijklmnopqr"));
+		assertThat(x19.asList().stream().collect(joining("")), is("abcdefghijklmnopqrs"));
+		assertThat(x20.asList().stream().collect(joining("")), is("abcdefghijklmnopqrst"));
+		// @formatter:on
+	}
+	
+	@Test
+	public void shouldCreateWithMoreElements()
+	{
+		// @formatter:off
+		final Plural<Integer> x21 = Plural.of(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21);
+		final Plural<Integer> x22 = Plural.of(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22);
+		final Plural<Integer> x23 = Plural.of(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23);
+		assertThat(x21.asList().stream().map($ -> $.toString()).collect(joining(",")), is("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21"));
+		assertThat(x22.asList().stream().map($ -> $.toString()).collect(joining(",")), is("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22"));
+		assertThat(x23.asList().stream().map($ -> $.toString()).collect(joining(",")), is("1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23"));
+		// @formatter:on
+	}
+	
+	@Test
+	public void shouldBuildFromArray()
+	{
+		final String[] s0 = {};
+		final String[] s1 = {"a"};
+		final String[] s2 = {"a", "b"};
+		final Plural<String> x0 = Plural.copy(s0);
+		final Plural<String> x1 = Plural.copy(s1);
+		final Plural<String> x2 = Plural.copy(s2);
+		assertThat(x0.asList().stream().collect(joining("")), is(""));
+		assertThat(x1.asList().stream().collect(joining("")), is("a"));
+		assertThat(x2.asList().stream().collect(joining("")), is("ab"));
+	}
+	
+	@Test
+	public void shouldBeImmutableAfterArrayConstructors()
+	{
+		// @formatter:off
+		final String[] strings = {"u","v","w"};
+		final Plural<String> x = Plural.of("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t", strings);
+		strings[1] = "1";
+		assertThat(x, is(Plural.of("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w")));
+		final Plural<String> y = Plural.copy(strings);
+		strings[2] = "2";
+		assertThat(y, is(Plural.of("u","1","w")));
+		// @formatter:on
+	}
+	
+	@Test
+	public void shouldCopyCollection()
+	{
+		final List<String> list = new ArrayList<>();
+		list.add("a");
+		list.add("b");
+		final Plural<String> x = Plural.copy(list);
+		assertThat(x.asList(), is(equalTo(list)));
+	}
+	
+	@Test
+	public void shouldCopyEmptyCollection()
+	{
+		final List<String> list = Collections.emptyList();
+		final Plural<String> x = Plural.copy(list);
+		assertTrue(x.asList().isEmpty());
+	}
+	
+	@Test
+	public void shouldCopyIterableAsCollection()
+	{
+		final Iterable<String> iterable = Arrays.asList("a", "b");
+		final Plural<String> x = Plural.copy(iterable);
+		assertThat(x.asList(), is(equalTo(iterable)));
+	}
+	
+	@Test
+	public void shouldBeImmutableAfterCopyConstructor()
+	{
+		final String[] strings = {"a", "b"};
+		final Iterable<String> list = Arrays.asList(strings);
+		final Plural<String> x = Plural.copy(list);
+		strings[1] = "c";
+		assertThat(x, is(Plural.of("a", "b")));
+	}
+	
+	@Test
+	public void shouldCopyNonCollectionIterable()
+	{
+		final Iterable<Character> iterable = () -> new Iterator<Character>()
+		{
+			int i = 0;
+			final String abc = "abc";
+			@Override public boolean hasNext() { return i < abc.length(); }
+			@Override public Character next() { return abc.charAt(i++); }
+		};
+		final Plural<Character> x = Plural.copy(iterable);
+		assertThat(x.asList().size(), is(3));
+		assertThat(x.toString(), containsString("a"));
+		assertThat(x.toString(), containsString("b"));
+		assertThat(x.toString(), containsString("c"));
+	}
+	
+	@Test
+	public void shouldCopyEmptyIterable()
+	{
+		final Iterable<String> emptyIterable = () -> new Iterator<String>()
+		{
+			@Override public boolean hasNext() { return false; }
+			@Override public String next() { throw new NoSuchElementException(); }
+		};
+		final Plural<String> x = Plural.copy(emptyIterable);
+		assertTrue(x.asList().isEmpty());
+	}
+	
+	@Test
+	public void shouldNeverEqualNull()
+	{
+		final Plural<String> x = Plural.of("a");
+		assertFalse(x.equals(null));
+	}
+	
+	@Test
+	public void shouldNeverEqualAnotherClass()
+	{
+		final List<String> list = Arrays.asList("a", "b", "c");
+		final Plural<String> x = Plural.of("a", "b", "c");
+		assertFalse(x.equals(list));
+	}
+}
