1d34bcba9572c448f723bcb32f24f7baede9348c
Will Hains <will@willhains.com>
2019-03-05 22:04:40 +0900

    Add documentation for new annotations.

--- src/com/willhains/purity/Released.java
+++ src/com/willhains/purity/Released.java
@@ -11,6 +11,17 @@ import static java.lang.annotation.ElementType.TYPE_USE;
  * Indicates that a method argument or return value is not retained (stored in a field) by the method, or by any methods
  * to which it is passed from inside the method.
  *
+ * Formally, the annotated argument / return value:
+ * <ol>
+ *     <li>Is not stored in a field of the method's class.</li>
+ *     <li>Is not stored directly in a field of another class.</li>
+ *     <li>Is not passed to a constructor or method as a {@link Retained} argument.</li>
+ *     <li>If passed to a constructor or method as a {@link Released} argument, the return value is released.</li>
+ * </ol>
+ *
+ * Method arguments and return values of {@link Pure} classes are {@link Released} by default, since pure values are
+ * immutable and therefore unable to retain them.
+ *
  * @see Retained
  * @see Released
  */
